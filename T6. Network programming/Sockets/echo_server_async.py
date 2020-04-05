5b07e0220c445457e6ab0165fa9c0decebd15c0a
tishyk <sytishchenko@gmail.com>
2020-04-06 00:15:00 +0300

    add comments

--- T6. Network programming/Sockets/echo_server_async.py
+++ T6. Network programming/Sockets/echo_server_async.py
@@ -2,33 +2,37 @@
 
 from socket import *
 import asyncio
-from pprint import  pprint
+from pprint import pprint
+
 
 async def echo_server(address, loop):
-    con = socket(AF_INET, SOCK_STREAM)
-    con.bind(address)
-    con.listen(2)
-    con.setblocking(False)
+    con = socket(AF_INET, SOCK_STREAM)  # Set TCP
+    con.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # Set reusable socket address
+    con.bind(address)  # Bind socket to (ip, port)
+    con.listen(5)  # Set maximum connections of connected clients
+    con.setblocking(False)  # True is equivalent to settimeout(None); False is equivalent to settimeout(0.0).
+
     print('Server started..')
 
     while True:
-        client, addr = await loop.sock_accept(con)
-        print("Connection from", addr)
-        future = loop.create_task(echo_handler(client))
+        client, addr = await loop.sock_accept(con)  # Accept connection from a single client
+        print("Connection from", addr)  # Show client ip address
+        future = loop.create_task(echo_handler(client))  # Asynchronous clients connections
+
 
 async def echo_handler(client):
-    while True:
-        if not loop.is_closed():
-            data = await loop.sock_recv(client, 10000)
+    while True:  # Create generator for yielding
+        if not loop.is_closed():  # Check for opened event loop
+            data = await loop.sock_recv(client, 10000)  # None blocking socket receiving task
             pprint(data)
-            asyncio.wait(print(i) for i in range(50000))
+            asyncio.wait(print(i) for i in range(50000))  # Set extra load for example
             if not data:
-                break
-            await loop.sock_sendall(client, b"Got: " + data)
+                break  # Exit if no client data send after successful connection
+            await loop.sock_sendall(client, b"Got: " + data)  # Send answer to client. Not blocking task
     print("Connection closed")
-    client.close()
+    client.close()  # Client connection closing
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(echo_server(('', 9000), loop))
+    loop = asyncio.get_event_loop()  # Create event loop to handle clients
+    loop.run_until_complete(echo_server(('', 9000), loop))  # Run server
