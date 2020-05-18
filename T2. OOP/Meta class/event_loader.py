79c8687cb8cc3856b1fbdc2292e93e6f7e5cff10
tishyk <sytishchenko@gmail.com>
2020-05-18 22:38:13 +0300

    update event loader with test data

--- T2. OOP/Meta class/event_loader.py
+++ T2. OOP/Meta class/event_loader.py
@@ -1,19 +1,25 @@
 import json
 
+
 def loader():
     json_dict_file = open("uz_bukovel_event.json", 'r', encoding='utf8')
     json_dict = json.loads(json_dict_file.read())
     return json_dict
 
-class Meta(type):
-    def __new__(cls, class_name,bases, clsdict):
+
+class MyType(type):
+    def __new__(cls, class_name, bases, clsdict):
         clsdict.update(loader())
         cls_obj = super().__new__(cls, class_name, bases, clsdict)
-        # cls_obj = super().__new__(cls, class_name, bases, loader() )
+        #cls_obj = super().__new__(cls, class_name, bases, loader() )
         return cls_obj
 
-class Event(metaclass=Meta):
+
+class Event(metaclass=MyType):
     data = 10
 
-event = Event()
-pass    # Debug it from here
+
+event1 = Event()
+event2 = Event()
+event3 = Event()
+pass  # Debug it from here
